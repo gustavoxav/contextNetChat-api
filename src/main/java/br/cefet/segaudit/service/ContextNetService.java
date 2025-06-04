72d12351656d5fcdfcf2d4abbdf8828dbefc3c83
Gustavo Xavier Saldanha <gustavosaldxav@gmail.com>
2025-06-04 18:42:02 -0300

    feat: adicionada nova configuração de lançamento para SegAudit e ajustes na classe ContextNetService.

--- src/main/java/br/cefet/segaudit/service/ContextNetService.java
+++ src/main/java/br/cefet/segaudit/service/ContextNetService.java
@@ -18,14 +18,17 @@ public class ContextNetService implements NodeConnectionListener {
 
     private MrUdpNodeConnection connection;
     private WebSocketSession currentSession;
-    private final UUID myUUID = UUID.randomUUID();
-    private final UUID destinationUUID = UUID.fromString("00000000-0000-0000-0000-000000000000"); // substitua
+    private final UUID myUUID = UUID.fromString("cc2528b7-fecc-43dd-a1c6-188546f0ccbf");
+    private final UUID destinationUUID = UUID.fromString("641f18ae-6c0c-45c2-972f-d37c309a9b72");
+    private static String gatewayIP = "bsi.cefet-rj.br";
+    private static int gatewayPort = 5500;
 
     public ContextNetService() {
         try {
-            InetSocketAddress address = new InetSocketAddress("127.0.0.1", 5500); // substitua conforme necessário
+            InetSocketAddress address = new InetSocketAddress(gatewayIP, gatewayPort);
             connection = new MrUdpNodeConnection(myUUID);
             connection.addNodeConnectionListener(this);
+            System.out.println("Conectando ao gateway " + myUUID + " e destino: " + destinationUUID);
             connection.connect(address);
         } catch (IOException e) {
             e.printStackTrace();
@@ -33,24 +36,37 @@ public class ContextNetService implements NodeConnectionListener {
     }
 
     public void registerSession(WebSocketSession session) {
-        this.currentSession = session;
-        sendToContextNet("Conexão registrada via WebSocket");
+        // this.currentSession = session;
+        // sendToContextNet("Conexão registrada via WebSocket");
     }
 
     public void unregisterSession(WebSocketSession session) {
-        if (session == this.currentSession) {
-            this.currentSession = null;
-        }
+        System.out.println("Desconectando sessão WebSocket: " + session.getId());
+        // if (session == this.currentSession) {
+        // this.currentSession = null;
+        // }
     }
 
     public void processIncomingMessage(WebSocketSession session, String payload) {
+        System.out.println("Processando mensagem recebida: " + payload);
         sendToContextNet(payload);
     }
 
     private void sendToContextNet(String payload) {
+
+        InetSocketAddress address = new InetSocketAddress(gatewayIP, gatewayPort);
+        try {
+            connection = new MrUdpNodeConnection(myUUID);
+            connection.addNodeConnectionListener(this);
+            connection.connect(address);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         ApplicationMessage appMessage = new ApplicationMessage();
         appMessage.setContentObject(payload);
         appMessage.setRecipientID(destinationUUID);
+        System.out.println("Enviando mensagem para ContextNet: " + payload + " para " + destinationUUID);
         try {
             connection.sendMessage(appMessage);
         } catch (IOException e) {
@@ -58,11 +74,13 @@ public class ContextNetService implements NodeConnectionListener {
         }
     }
 
-    // Recebendo mensagens do ContextNet
     @Override
     public void newMessageReceived(NodeConnection remoteCon, Message message) {
         try {
+            System.out.println("Confirmação: " + message.getContentObject());
+
             String content = (String) message.getContentObject();
+            System.out.println("Mensagem recebida do ContextNet: " + content);
             if (currentSession != null && currentSession.isOpen()) {
                 currentSession.sendMessage(new TextMessage("Recebido do ContextNet: " + content));
             }
@@ -71,9 +89,16 @@ public class ContextNetService implements NodeConnectionListener {
         }
     }
 
-    // Outros métodos obrigatórios da interface
     @Override
     public void connected(NodeConnection remoteCon) {
+        ApplicationMessage message = new ApplicationMessage();
+        message.setContentObject("Registering");
+
+        try {
+            connection.sendMessage(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
